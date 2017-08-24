8a6511114d684f6cc2ae847d5ac8ca95ff7010a8
Daniel Gawne <danielgawne@gmail.com>
2017-08-24 16:48:09 -0400

    Solved and added problem 1

---
+++ euler1.js
@@ -0,0 +1,18 @@
+var numList = [];
+var upTo = 10;
+for (var i = 0; i < upTo; i++) {
+  if (i % 3 === 0 && i > 0) {
+    numList.push(i);
+    console.log('Added ' + i);
+  } else if (i % 5 === 0 && i > 0) {
+    numList.push(i);
+    console.log ('Added ' + i);
+  } else{};
+}
+var length = numList.length;
+console.log(numList);
+var sum = 0;
+for (var i in numList) {
+  sum += numList[i];
+}
+console.log('The sum of the contents of numList is ' + sum);
